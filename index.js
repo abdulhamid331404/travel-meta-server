7a2a2230b1dc4d8c77744414e4d524932df31a6e
abdulhamid331404 <freelancerhamid1@gmail.com>
2022-11-11 20:32:35 +0600

    update data

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// user: travelDBUser
-// pass: yQ7bgxLvqx0ofmr1
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mlgekjs.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
