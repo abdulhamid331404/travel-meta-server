b88bc0de8d9933fd7c3c462d1dcc2ce5d1ab58b9
abdulhamid331404 <freelancerhamid1@gmail.com>
2022-11-11 18:57:16 +0600

    create single id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -19,13 +19,18 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run(){
 try{
-const traverServiceCollection = client.db('travelMeta').collection('services');
+const travelServiceCollection = client.db('travelMeta').collection('services');
 
 app.get('/services', async(req, res)=>{
 const query = {};
-const cursor = traverServiceCollection.find(query);
+const cursor = travelServiceCollection.find(query);
 const services = await cursor.toArray();
 res.send(services);
+});
+
+app.get('/services/:id', async(req, res) =>{
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
 })
 
 }
