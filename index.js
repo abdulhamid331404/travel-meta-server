3c198223d83c2f60195fcb338b109b8c963d2d3d
abdulhamid331404 <freelancerhamid1@gmail.com>
2022-11-11 09:01:57 +0600

    add to database

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors());
 app.use(express.json());
+
+// user: travelDBUser
+// pass: yQ7bgxLvqx0ofmr1
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mlgekjs.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 
 app.get('/', (req, res)=>{
     res.send('travel meta server in running');
